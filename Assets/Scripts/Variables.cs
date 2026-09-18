bbc0eb12b6d98d1bc59c629c65642715ead110ce
camis233 <camila.ps4@puccampinas.edu.br>
2026-09-18 10:20:45 -0300

    Repetição/loop

--- Assets/Scripts/Variables.cs
+++ Assets/Scripts/Variables.cs
@@ -1,8 +1,14 @@
+using NUnit.Framework.Internal;
+using System.Security.Cryptography;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Variables : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    enum Itens { Espada, Escudo, Pocao, Luva, Armadura, Bota };
     void Start()
     {
         int myNumber;
@@ -66,6 +72,126 @@ public class Variables : MonoBehaviour
         inventorySlot[0] = "Fim do mundo";
 
         int size = inventorySlot.Length;
+
+        //Escopo: é o limite ou contexto que define onde uma variável, função ou objeto pode ser visto e acessado no código
+
+        {
+
+            int temp1 = 0;
+            int temp2 = 0;
+            { // escopo Depois start
+                int temp3 = 2;
+                {
+                    int temp42 = 5;
+                    temp3 = 5;
+                }
+                //temp42 = 7;
+                //temp4 = 89898;
+            }
+
+            temp1 = 1;
+            temp2 = 2;
+            //temp3 = 3;
+            int temp4 = 2;
+            int temp5 = 2;
+
+        }
+
+        // Criando uma condição no código com base no jogo Sonic
+
+        int rings = 5;
+        bool isJumpSuccess = true;
+        bool hasShield = false;
+
+        if(isJumpSuccess ==true) // Se o meu pulo for bem sucedido
+
+        {
+            rings += 3; // Eu ganho 3 moedas
+        }
+        else if(isJumpSuccess == false && hasShield == false) //porém, se meu pulo for mal sucedido
+        {
+            rings = 0; // Eu perco todas as moedas
+        }
+        else if (isJumpSuccess == false && hasShield == true)
+        {
+            Debug.Log("Retornou em perder anéis");
+        }
+        else //Se nenhuma ação for realizada a condição será que ele não está pulando
+        {
+            Debug.Log("Não estou pulando");
+
+           
+
+
+            //Aula, dia 18/09/2026
+
+    Itens currentitem = Itens.Espada;
+    int index = (int)currentitem;
+
+            if (currentitem == Itens.Espada)
+            {
+                Debug.Log("Prosnagem usa Espada");
+            }
+            else if (currentitem == Itens.Escudo)
+            {
+                Debug.Log("Personagem usa Escudo");
+            }
+            else if (currentitem == Itens.Armadura)
+            {
+                Debug.Log("Personagem usa Armadura");
+            }
+
+            char teste = ('a');
+            switch (teste)
+            {
+                case 'a':
+                    {
+                        string nomePersonagem = "Cesar";
+                        Debug.Log("Personagem " + nomePersonagem + " usa Espada");
+                    }
+                    break;
+                case 'b':
+                case 'c':
+                case 'd':
+                    Debug.Log("Agora b, c e d possuem o mesmo código!!!");
+                    break;
+                case 'e':
+                    Debug.Log("Personagem usa Armadura");
+                    break;
+                case 'f':
+                    Debug.Log("Personagem usa Bota");
+                    break;
+                default:
+                    break;
+            }
+
+            int aleatorio = Random.Range(0, 15);
+            float aleatorio2 = Random.Range(0.0f, 15.0f);
+
+
+
+            string[] inventario = { "Espada", "Escudo", "Armadura", "Luva", "Bota", "Elmo", "Pocao", "Adaga", "Kit Medico", "Livro" };
+
+
+            Random.Range(0, inventario.Length);
+
+
+            for (int i = 0;i < inventario.Length; i++) 
+            {
+              if (inventario[i]== "Armadura de Prata")
+                {
+                    Debug.Log("Este inventario possui um arma mítica!!!!");
+                    break;
+                }
+                //Debug.Log(inventario[i]);
+            }
+           
+
+
+
+
+
+        }
 
     }
 }
